0dd297aecf1ce47c15253b8a2f1d62bc5988aa04
npotts <npotts@users.noreply.github.com>
2021-08-19 22:34:38 -0600

    use soc.h

--- main/atm90e32.c
+++ main/atm90e32.c
@@ -13,7 +13,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <math.h>
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
-#include "soc/spi_caps.h"
+#include "soc/soc.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
